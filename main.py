c23f241932fb43d83c0470e114b356ec68bbb781
unknown <Nobel.kwee@gmail.com>
2025-02-20 16:36:12 +0700

    menambahkan fungsi pembagian

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ def pengurangan(a,b):
 	total = a-b
 	return(total)
 
+def pembagian(a,b):
+	total = a/b
+	return(total)
+
 def main():
+	print(pembagian(200,10))
 	print(perkalian(10,10))	
 	print(penambahan(10,5))
 	print(pengurangan(1,2))
